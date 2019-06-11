1477cbed37d5014c4e81b4b451a39de176b8d078
han.congcong <han.congcong@immomo.com>
2019-06-11 18:36:59 +0800

    add readme

--- src/main/java/com/hc/bean/annotation/BeanValueGetter.java
+++ src/main/java/com/hc/bean/annotation/BeanValueGetter.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * bean内值的getter
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface BeanValueGetter {
